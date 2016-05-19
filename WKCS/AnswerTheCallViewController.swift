3ff8168d422582bc3dc86f41c0269fc1ccbe7f69
Bryan Dunbar <bryandunbar@gmail.com>
2016-05-18 23:48:40 -0400

    answer the call working

--- WKCS/AnswerTheCallViewController.swift
+++ WKCS/AnswerTheCallViewController.swift
@@ -7,21 +7,81 @@
 //
 
 import UIKit
+import AVFoundation
 
-class AnswerTheCallViewController: UIViewController {
+class AnswerTheCallViewController: UIViewController, AVAudioPlayerDelegate {
 
+    
+    var player:AVAudioPlayer?
+    
+    @IBOutlet weak var answerCallButton: UIButton!
+    @IBOutlet weak var proceedButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), {
+            self.playPhoneRinging()
+        })
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func playPhoneRinging() {
+        let phoneRinging = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("phone-ringing", ofType: "mp3")!)
+        do {
+            try player = AVAudioPlayer(contentsOfURL: phoneRinging)
+            player!.prepareToPlay()
+            player!.numberOfLoops = 2
+            player!.play()
+        }  catch _ {
+            
+        }
 
+    }
+    
+    func playTheCall() {
+        
+        if let player = self.player {
+            if player.playing {
+                player.stop()
+            }
+        }
+        
+        let phoneRinging = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("phone-ringing", ofType: "mp3")!)
+        do {
+            try player = AVAudioPlayer(contentsOfURL: phoneRinging)
+            player!.prepareToPlay()
+            player!.delegate = self
+            player!.numberOfLoops = 1
+            player!.play()
+        }  catch _ {
+            
+        }
+    }
+    
+    @IBAction func answerCallTapped(sender: AnyObject) {
+        self.playTheCall()
+        self.answerCallButton.enabled = false
+    }
+    
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully flag: Bool) {
+        if flag {
+            self.answerCallButton.setTitle("Replay the Call", forState: UIControlState.Normal)
+            self.answerCallButton.enabled = true
+            self.proceedButton.hidden = false
+        }
+    }
+    
+    
     /*
     // MARK: - Navigation
 
